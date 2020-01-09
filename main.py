42efbb8f2f3e856c6a6f3af6cdb5891796aefc45
cjuracek <therealjuracek@gmail.com>
2020-01-09 15:54:50 -0500

    Abstracted scraping songs for a DDR game into function. Computed set difference between 2 games.

--- main.py
+++ main.py
@@ -2,16 +2,21 @@
 # DDR Supernova: .mw-parser-output > ul a:nth-child(1)
 
 import requests
-from selenium import webdriver
 from bs4 import BeautifulSoup
+
+def get_songs_from_url(url):
+    r = requests.get(url).content
+    soup = BeautifulSoup(r)
+    song_info = soup.select('.mw-parser-output > ul a:nth-child(1)')
+    song_names = {song.contents[0] for song in song_info}
+    return song_names
 
 if __name__ == "__main__":
 
-    supernova_url = 'https://remywiki.com/DanceDanceRevolution_SuperNOVA_Full_Song_List#DanceDanceRevolution_Solo_Series_.288_Songs.29'
+    supernova_url = 'https://remywiki.com/DanceDanceRevolution_SuperNOVA_Full_Song_List'
+    a_url = 'https://remywiki.com/DanceDanceRevolution_A_Full_Song_List'
 
-    r = requests.get(supernova_url).content
-    soup = BeautifulSoup(r)
-    songs = soup.select('.mw-parser-output > ul a:nth-child(1)')
-    songs = [song.contents[0] for song in songs]
+    supernova_songs = get_songs_from_url(supernova_url)
+    a_songs = get_songs_from_url(a_url)
 
-    x = 4
+    print("\n".join(supernova_songs - a_songs))
